4023ffafcc2b2a0b24649a075db04de637c409d1
Enrique Avalos <keex@Enriques-Air.lan>
2023-07-22 01:20:25 -0700

    fixed node issue with 1st line

--- script2.js
+++ script2.js
@@ -1,55 +1,59 @@
-// NOTE: had to use readline because prompt() wasn't working in Node anymore
+const prompt = require("prompt-sync")();
 
-const readline = require('readline');
-
-const target = Math.round(Math.random() * 90);
-console.log(target);
+const target = Math.round(Math.random() * 90)
+console.log(target)
 
 let guesses = 0;
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
-function getUserInput() {
-  rl.question("Guess the number between 0-100: ", function (question) {
-    const guess = Number(question);
+// const guess = Number(question);
+
+while(true) {
     guesses++;
-
-    if (guess > target) {
-      console.log("Your guess is too high.");
-      getUserInput(); // Ask for input again
-    } else if (guess < target) {
-      console.log("Your guess is too low.");
-      getUserInput(); // Ask for input again
-    } else {
-      console.log("You guessed it!");
-      console.log("You guessed the number in", guesses, "tries!");
-      rl.close();
-    }
-  });
+const guess = Number(prompt("Guess the number between 0-100: "));
+if(guess > target) {
+    console.log("Your guess it too high.");
+} else if (guess < target) {
+    console.log("Your guess is too low.");
+} else {
+    console.log("You guessed it!");
+    break;
 }
+}
+console.log("You guessed the number in", guesses, "tries!")
 
-getUserInput(); // Start asking for user input
 
 
-// const target = Math.round(Math.random() * 90)
-// console.log(target)
+// NOTE: had to use readline because prompt() wasn't working in Node anymore
+
+// const readline = require('readline');
+
+// const target = Math.round(Math.random() * 90);
+// console.log(target);
 
 // let guesses = 0;
-// const guess = Number(prompt("Guess the number between 0-100: "));
-// // const guess = Number(question);
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-// while(true) {
+// function getUserInput() {
+//   rl.question("Guess the number between 0-100: ", function (question) {
+//     const guess = Number(question);
 //     guesses++;
 
-// if(guess > target) {
-//     console.log("Your guess it too high.");
-// } else if (guess < target) {
-//     console.log("Your guess is too low.");
-// } else {
-//     console.log("You guess it!");
-//     break
+//     if (guess > target) {
+//       console.log("Your guess is too high.");
+//       getUserInput(); // Ask for input again
+//     } else if (guess < target) {
+//       console.log("Your guess is too low.");
+//       getUserInput(); // Ask for input again
+//     } else {
+//       console.log("You guessed it!");
+//       console.log("You guessed the number in", guesses, "tries!");
+//       rl.close();
+//     }
+//   });
 // }
-// }
-// console.log("You guessed the number in", guesses, "tries!")
+
+// getUserInput(); // Start asking for user input
+
