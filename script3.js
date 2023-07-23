d73a99238f5a6146e7fd03906927caa06eaea898
Enrique Avalos <keex@Enriques-Air.lan>
2023-07-23 14:18:46 -0700

    rock, paper, scissors

---
+++ script3.js
@@ -0,0 +1,50 @@
+const prompt = require("prompt-sync")();
+
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
+while(true) {
+const playerChoice = prompt("Enter rock, paper, scissors (or q to quit): ")
+if (playerChoice.toLowerCase() === "q") {
+    break;
+}
+
+if 
+(playerChoice !== "rock" && 
+playerChoice !== "paper" && 
+playerChoice !== "scissors"
+) 
+{
+    console.log("Please make a valid playerChoice.")
+    continue;
+} 
+
+const choices = ["rock", "paper", "scissors"];
+const randomIndex = Math.round(Math.random() * 2);
+const computerChoice = choices[randomIndex];
+console.log("The computer chose: ", computerChoice);
+
+if (computerChoice === playerChoice) {
+    console.log("Draw!");
+    ties++
+} else if (
+(playerChoice === "paper" && computerChoice === "rock") ||
+          (playerChoice === "rock" && computerChoice === "scissors") ||
+          (playerChoice === "scissors" && computerChoice === "paper")
+)
+ {
+    console.log("You Won!");
+    wins++
+} else {
+    console.log("You Lost!");
+}
+}
+console.log("Wins: ", wins, "Losses: ", losses, "Ties: ", ties)
+
+
+// if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+
+// } else {
+//     console.log("please make playerChoice")
+// }
